f0ff0830869eb12830d2f6ce087584a3c5fe7157
สึคุนะ <prem0865079888@gmail.com>
2025-01-28 17:22:36 +0700

    Update TextInp.jsx

--- client/src/components/user/TextInp.jsx
+++ client/src/components/user/TextInp.jsx
@@ -1,14 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
+import { LuEye, LuEyeClosed } from "react-icons/lu";
 
 function TextInp({ Icon, type, placeholder }) {
+  const [showPassword, setShowPassword] = useState(false);
+
+  // สลับสถานะแสดง/ซ่อนรหัสผ่าน
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
+
   return (
     <div className="w-full h-12 flex items-center bg-[#f5f5f5] px-4 rounded-lg focus-within:ring-2 focus-within:ring-sea-blue ">
       {Icon && <Icon className="text-[22px] mr-2 opacity-40" />}
       <input
-        type={type}
+        type={type === "password" && showPassword ? "text" : type}
         placeholder={placeholder}
         className="w-full outline-none bg-[#f5f5f5]"
       />
+      {type === "password" && (
+        <button
+          type="button"
+          onClick={togglePasswordVisibility}
+          className="ml-2 opacity-40 focus:outline-none"
+        >
+          {showPassword ? (
+            <LuEye className="text-[22px]" /> // ไอคอนซ่อนรหัสผ่าน
+          ) : (
+            <LuEyeClosed className="text-[22px]" /> // ไอคอนแสดงรหัสผ่าน
+          )}
+        </button>
+      )}
     </div>
   );
 }
