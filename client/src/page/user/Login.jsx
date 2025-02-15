1f92c3ff07066477d6598935252f3aa1bb01e278
สึคุนะ <prem0865079888@gmail.com>
2025-02-15 17:12:51 +0700

    Update Login.jsx
    connect api

--- client/src/page/user/Login.jsx
+++ client/src/page/user/Login.jsx
@@ -1,12 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import UserInp from "../../components/user/UserInp";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { LuUser, LuLock } from "react-icons/lu";
 import { FaArrowLeftLong } from "react-icons/fa6";
+import { loginUser } from "../../utils/func/apiService";
+import { Toaster, toast } from "react-hot-toast";
 
 function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  // ฟังก์ชันเข้าสู่ระบบ
+  const handleLogin = async () => {
+    if (!username || !password) {
+      toast.error("กรุณากรอกชื่อผู้ใช้และรหัสผ่าน");
+      return;
+    }
+
+    toast.promise(
+      loginUser(username, password), // เรียก API Login
+      {
+        loading: "กำลังเข้าสู่ระบบ...",
+        success: (res) => {
+          localStorage.setItem("token", res.token); // บันทึก Token ใน LocalStorage
+          localStorage.setItem("role", res.role); // บันทึก Role
+          setTimeout(() => navigate("/"), 2000); // กลับไปหน้าแรก
+          return "เข้าสู่ระบบสำเร็จ!";
+        },
+        error: (err) => `${err.message || "เกิดข้อผิดพลาด"}`,
+      }
+    );
+  };
   return (
     <div className="h-screen w-full text-grey bg-gradient-to-b from-sea-blue to-green-pastel flex flex-col items-center justify-center gap-7">
+      <Toaster position="top-center" reverseOrder={false} />
       <div className="bg-white w-[485px] p-12 rounded-xl flex flex-col gap-7 shadow-lg">
         <div>
           <p className="text-[28px] font-semibold">
@@ -15,10 +43,25 @@ function Login() {
         </div>
         <div className="space-y-4">
           {/* username */}
-          <UserInp Icon={LuUser} type="text" placeholder="ชื่อผู้ใช้" />
+          <UserInp
+            Icon={LuUser}
+            type="text"
+            placeholder="ชื่อผู้ใช้"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
           {/* password */}
-          <UserInp Icon={LuLock} type="password" placeholder="รหัสผ่าน" />
-          <button className="w-full h-12 bg-primary text-white rounded-lg">
+          <UserInp
+            Icon={LuLock}
+            type="password"
+            placeholder="รหัสผ่าน"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button
+            onClick={handleLogin}
+            className="w-full h-12 bg-primary text-white rounded-lg"
+          >
             เข้าสู่ระบบ
           </button>
         </div>
