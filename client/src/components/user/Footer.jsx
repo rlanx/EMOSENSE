95f87b43aa21338781c07144fa16d6299ad2ee39
สึคุนะ <prem0865079888@gmail.com>
2025-03-09 17:31:49 +0700

    Update -> Footer.jsx

--- client/src/components/user/Footer.jsx
+++ client/src/components/user/Footer.jsx
@@ -1,7 +1,43 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import footerLinks from "../../utils/json/footer";
+import { getAllNews, getAllResearch } from "../../utils/func/adminService";
+import { Link } from "react-router-dom";
 
 function Footer() {
+  const [newsList, setNewsList] = useState([]);
+  const [researchList, setResearchList] = useState([]);
+
+  const limitedNews = newsList.slice(0, 4);
+  const limitedResearch = researchList.slice(0, 4);
+
+  console.log(limitedNews);
+  console.log(limitedResearch);
+
+  // ดึงข้อมูลงานวิจัย
+  useEffect(() => {
+    getAllResearch()
+      .then((data) => setResearchList(data))
+      .catch((error) => toast.error(`${error.message}`));
+  }, []);
+
+  // ดึงข้อมูลข่าวสาร
+  useEffect(() => {
+    getAllNews()
+      .then((data) => setNewsList(data))
+      .catch((error) => toast.error(`${error.message}`));
+  }, []);
+
+  const homeLinks = [
+    {
+      title: "ความรู้ทั่วไปและข่าวสารเกี่ยวกับโรคซึมเศร้า",
+      path: "/news",
+    },
+    {
+      title: "บทความงานวิจัยเกี่ยวกับโรคซึมเศร้า",
+      path: "/research",
+    },
+  ];
+
   return (
     <div className="bg-sea-blue w-full lg:h-[350px] text-white lg:py-[35px]">
       {/* container */}
@@ -12,26 +48,54 @@ function Footer() {
           </div>
           <p>
             ระบบวิเคราะห์แนวโน้มโรคซึมเศร้าจากข้อความด้วย AI
-            ช่วยประเมินความเสี่ยงจากโพสต์หรือข้อความสะท้อนอารมณ์
+            ช่วยประเมินความเสี่ยง
+            <br />
+            จากโพสต์หรือข้อความสะท้อนอารมณ์
+            <br />
             เพื่อสนับสนุนสุขภาพจิตของคุณและคนใกล้ตัว
           </p>
         </div>
-        {footerLinks.map((section, index) => (
-          <div key={index}>
-            <h2 className="text-[24px] border-b-2 pb-2 mb-2">
-              {section.title}
-            </h2>
-            <ul className="space-y-2">
-              {section.links.map((link, ind) => (
-                <li key={ind}>
-                  <button>
-                    <span className="truncate">{link}</span>
-                  </button>
-                </li>
-              ))}
-            </ul>
-          </div>
-        ))}
+
+        <div>
+          <h2 className="text-[24px] border-b-2 pb-2 mb-2">หน้าหลัก</h2>
+          <ul className="space-y-2">
+            {homeLinks.map((link, ind) => (
+              <li key={ind}>
+                <Link to={link.path} className="truncate">
+                  {link.title}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+
+        <div>
+          <h2 className="text-[24px] border-b-2 pb-2 mb-2">
+            ความรู้ทั่วไป / ข่าวสาร
+          </h2>
+          <ul className="space-y-2">
+            {limitedNews.map((link, ind) => (
+              <li key={ind}>
+                <Link to={`/news/${link.news_id}`} className="truncate">
+                  {link.title}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+
+        <div>
+          <h2 className="text-[24px] border-b-2 pb-2 mb-2">บทความงานวิจัย</h2>
+          <ul className="space-y-2">
+            {limitedResearch.map((link, ind) => (
+              <li key={ind}>
+                <Link to={`/research/${link.research_id}`} className="truncate">
+                  {link.title}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
       <div className="w-[90%] border-t-2 mx-auto mt-[70px] py-[20px]">
         <p className="text-lg text-center">
